1db3ff71828a40e5b4f4c6473e898089d72312bb
Victor Omoniyi <vomoniyi2001@gmail.com>
2023-06-15 21:31:56 +0100

    Initialization

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,7 +3,11 @@ import './Home.scss'
 const Home = () => {
     return (
         <div className="home">
-        <h1>Homedeeqd2wd</h1>
+            <div className="home__left">
+                <h1>Email for developers</h1>
+                <p>The best API to reach humans instead of spam folders. Build, test, and deliver transactional emails at scale.</p>
+            </div>
+            <div className="home__right"></div>
         </div>
     )
 }
